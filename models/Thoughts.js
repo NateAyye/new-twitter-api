78419002533572c8b0f6d83c00670904d89648a0
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-03 20:07:47 -0700

    Created the Thoughts Model for Mongo and Touched up the user model just a little bit

---
+++ models/Thoughts.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+const { reactionSchema } = require('./Reaction');
+
+const thoughtSchema = new Schema({
+  thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
+  createdAt: {
+    type: Date, default: Date.now, get: (timestamp) => new Date(timestamp).toLocaleDateString('en-US', {
+      dateStyle: 'medium'
+    })
+  },
+  username: { type: String, required: true },
+  reactions: [reactionSchema]
+
+}, {
+  toJSON: {
+    virtuals: true,
+    getters: true
+  }
+})
+
+thoughtSchema.virtual('reactionCount').get(() => this.reactions.length);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,26 +1,3 @@
-// username --
-
-// String
-// Unique
-// Required
-// Trimmed
-// --
-
-// email --
-
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-//  --
-// thoughts
-// Array of _id values referencing the Thought model
-
-// friends
-// Array of _id values referencing the User model (self-reference)
-
-// Schema Settings:
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 const { Schema, model } = require('mongoose');
 
 
@@ -31,14 +8,13 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true, match: [emailRegEx] },
   thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought' }],
   friends: [{ type: Schema.Types.ObjectId, ref: 'user' }]
-
-},{
+}, {
   toJSON: {
     virtuals: true,
   }
 })
 
-userSchema.virtual('friendCount').get(() => this.friends.length); 
+userSchema.virtual('friendCount').get(() => this.friends?.length);
 
 const User = model('user', userSchema);
 
