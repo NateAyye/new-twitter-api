32c636ac1dca484061810160dd11d918f4e58734
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-03 20:14:41 -0700

    Forgot to add export in the Reactions.js model file

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,11 @@
 
 const { Schema } = require('mongoose');
 
-export const reactionSchema = new Schema({
+const reactionSchema = new Schema({
   reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
   reactionBody: { type: String, required: true, maxlength: 280 },
   username: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, get: (timestamp) => new Date(timestamp).toLocaleDateString('en-US', { dateStyle: 'medium' }) }
 })
 
-
+module.exports = reactionSchema;
