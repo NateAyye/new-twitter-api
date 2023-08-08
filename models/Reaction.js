28e542e8cb541d2518a2fc63067391d12c062099
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-07 21:47:50 -0700

    Added Types import from mongoose so schema could properly work

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,5 @@
 
-const { Schema } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema({
   reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
