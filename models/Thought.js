bf21c9d600215daf25905bc709afd5e72a71bb60
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-07 18:01:10 -0700

    Touched up the Thought model just a bit

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const { reactionSchema } = require('./Reaction');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema({
   thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
